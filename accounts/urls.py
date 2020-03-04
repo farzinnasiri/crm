c3ea05446968cc7b6b33ac792851c50d08d1a2c8
farzinnasiri <farzinnasiri890@gmail.com>
2020-03-04 19:43:09 +0330

    Initial commit

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from accounts import views
+
+urlpatterns = [
+    path('', views.home),
+    path('products/', views.products),
+    path('customer/', views.customer),
+]
